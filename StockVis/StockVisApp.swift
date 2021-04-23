c51aff5352c0e27d42207332c9aa6ce23975c3fa
rynnchan <ogw334@gmail.com>
2021-04-23 22:12:46 +0900

    Initial Commit

---
+++ StockVis/StockVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockVisApp.swift
+//  StockVis
+//
+//  Created by 小川凜人 on 2021/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StockVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

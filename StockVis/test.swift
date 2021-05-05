ffbf9a7e3cb772b767bf1f6350b4bc5385bf2333
kyuuki83 <kyuuki19980803@gmail.com>
2021-05-05 13:35:46 +0900

    testdayo

---
+++ StockVis/test.swift
@@ -0,0 +1,22 @@
+//
+//  test.swift
+//  StockVis
+//
+//  Created by 小中祐希 on 2021/05/05.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text("テストだよん")
+            .bold()
+            .border(Color.red)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
